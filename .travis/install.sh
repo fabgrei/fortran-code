720d2c2c5d51541607f2d7cf3fc1c75f65235031
Fabian Greimel <fabgrei@gmail.com>
2017-03-28 16:52:17 -0400

    brew gcc i/o gfortran

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,7 +4,7 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 
     # Install some custom requirements on OS X
     # e.g. brew install pyenv-virtualenv
-    brew install gfortran
+    brew install gcc
 
 else
     sudo apt-get install gfortran
